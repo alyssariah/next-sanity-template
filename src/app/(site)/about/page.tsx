70ce4fbb1a290a9573122aaa8b7620cb43f84211
alyssariah <alyssariah@gmail.com>
2023-06-13 22:02:36 -0400

    update lock file

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -10,7 +10,7 @@ export default async function AboutPage() {
 
   return (
     <div className="w-[100%] flex flex-col items-center">
-      <div className="max-w-[1440px] w-[100%] min-h-[calc(100vh-60px)] my-4 text-black-500">
+      <div className="max-w-[1024px] w-[100%] min-h-[calc(100vh-60px)] my-4 text-black-500">
         <SanityPortableText content={aboutContent?.content} />
       </div>
     </div>
