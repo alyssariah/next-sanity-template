c05599d999811c4ac0b431e47035dfc776c0f43b
alyssariah <alyssariah@gmail.com>
2023-06-18 21:47:37 -0400

    setup nextauth with sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -3,7 +3,8 @@ import blogPosts from './blog-posts';
 import home from './home';
 import about from './about';
 import contact from './contact';
+import { user, account, verificationToken } from 'next-auth-sanity/schemas';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [home, about, contact, blogPosts],
+  types: [home, about, contact, blogPosts, user, account, verificationToken],
 };
