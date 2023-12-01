ebc702e34c3b4e8f84b0989450e92fe9e71bff24
alyssariah <alyssariah@gmail.com>
2023-11-30 20:08:56 -0500

    update package.json and use pnpm instead

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['next-auth-sanity'],
+  },
   images: {
     domains: ['cdn.sanity.io']
   },
