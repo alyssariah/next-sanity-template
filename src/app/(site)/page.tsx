8da150c429e2b1edf3542f3b9c9a5729617b8164
alyssariah <alyssariah@gmail.com>
2024-05-21 14:25:00 -0400

    updates to packages

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -10,7 +10,7 @@ import { BsTagFill } from 'react-icons/bs';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../lib/auth';
 
-export default async function Home() {
+export default async function HomePage() {
   const session = await getServerSession(authOptions);
   const homeContent = await getHomePage();
 
